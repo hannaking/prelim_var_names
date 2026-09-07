4143a9fd29132dadb8cab5136d271ccd26d6c30b
mandatory-username <106693160+hannaking@users.noreply.github.com>
2026-09-07 14:04:38 -0400

    moved output to file, minor fixes in rules

--- src/main.py
+++ src/main.py
@@ -6,11 +6,22 @@ filename = 'sample.py'
 entry = Submission(filename)
 
 result = runpy.run_path('rules.py')
+
+#exec(open("rules.py").read()) # doesn't seem particularly faster than runpy? suggestions?
+
 registry = result["registry"]
 
-run_namesake([i.value for i in entry.identifiers])
-
+#run_namesake([i.value for i in entry.identifiers])
+#print()
+output : str = ""
 for i in entry.identifiers:
     # run the rules from the registry
     for key in registry.registry:
-        registry.call(key, i)
+        text_from_rule : str = registry.call(key, i)
+        if text_from_rule is not None:
+            output += (f'Name misaligned with type; {key}; {text_from_rule}\n')
+
+
+# diagnosis, rule, identifier into a text file (seperated by ;)
+with open('../data/report.txt', 'w') as file:
+    file.write(output)
--- src/rules.py
+++ src/rules.py
@@ -29,25 +29,25 @@ registry = RuleRegistry()
 def same_as_type(i : Identifier):
     name = i.value
     if name.lower() == i.types[0].lower():
-        print(f'Do not use the type, {i.types[0]}, as the name for {name}.')
+        return f'Do not use the type, {i.types[0]}, as the name for {name}.'
 
 @registry.register("type in name")
 def type_in_name(i : Identifier):
     name = i.value
     if i.types[0].lower() in name.lower():
-        print(f'Do not use the type, {i.types[0]}, as the name for {name}.')
+        return f'Do not use the type, {i.types[0]}, as the name for {name}.'
 
 @registry.register("plural for collections")
 def plural_for_collections(i : Identifier):
     name = i.value
     if i.types[0] in constants.COLLECTION_TYPES and not is_plural(name):
-        print(f'Your variable {name} is a collection type {i.types[0]}, but {name} is singular. Collection type variables may hold multiple values and should therefore be plural.')
+        return f'Your variable {name} is a collection type {i.types[0]}, but {name} is singular. Collection type variables may hold multiple values and should therefore be plural.'
 
 @registry.register("singular for noncollections")
-def singluar_non_collections(i : Identifier):
+def singular_non_collections(i : Identifier):
     name = i.value
     if i.types[0] not in constants.COLLECTION_TYPES and is_plural(name):
-        print(f'Your variable {name} is a singular type {i.types[0]}, but {name} is plural. Non-collection type variables hold only one value at a time and should therefore be singular.')
+        return f'Your variable {name} is a singular type {i.types[0]}, but {name} is plural. Non-collection type variables hold only one value at a time and should therefore be singular.'
 
 def is_plural(name : str) -> bool:
     tokens = nltk.word_tokenize(name)
@@ -59,8 +59,8 @@ def is_plural(name : str) -> bool:
 def boolean_prefix_present(i : Identifier):
     name = i.value
     words : list[str] = name.split('_')
-    if i.types[0] == bool and words[0] not in constants.BOOLEAN_PREFIX:
-        print(f'Your boolean variable {name} should include a verb prefix. Example prefixes include is, was, should, can, did, and had.')
+    if i.types[0] == "bool" and words[0] not in constants.BOOLEAN_PREFIX:
+        return f'Your boolean variable {name} should include a verb prefix. Example prefixes include is, was, should, can, did, and had.'
 
 #@registry.register("dictionary_words")
 #def use_dictionary_words(i : Identifier):
